05b2d973289a36c777a05910e629c24b4ed4d768
Sigit <dewa19@gmail.com>
2020-09-04 13:04:06 -0500

    #1. add helper, #2. modularize the functions, and #3. Update README.md

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 # LookupEmail
 
-This is GenServer version of *EmailLookUp* application that previously uploaded.
+**LookEmail** GenServer-based SMTP email look-up application. The GenServer process run on its own without Supervisor.
 
-Also, this project is part of my journey to understanding Elixir.
-The objective of this mini project is to validate an email address from the side of SMTP server (without actually sending an email). If an email is acknowledged by its SMTP server, then it is an "valid" email.
+The objective of this project is to validate an email address from the side of SMTP server (without really sending an email). If an email is acknowledged by its SMTP server, then it is an "valid" email.
 
-In SMTP protocol, there is a special command/syntax as a part of sending an email, ie: RCPT TO:\<email@example.com\>.
-If a response code of that command is like "250 OK" then the specified email is a valid one. Otherwise, it might be an invalid email (for any reason).
+In SMTP protocol, there is a special command to establish a recipient of the message, ie: RCPT TO:\<email@example.com\>. If the response code of that command is like "250 OK" then the specified email is a valid one. Otherwise, it might be an invalid email (for any reason).
 
-With that mechanism, we wan make an "educated guest" about the status of an email (exist or not). 
+With that mechanism, we wan make an "educated guess" about the status of an email (exist or not exist).
+
+## Installing
+
+After clone this application, you can start right away. Don't forget to start GenServer first by calling ```LookupEmail.start_link```.
+
+  <your_machine/lookup_email>$ iex -S mix
+  Erlang/OTP 23 [erts-11.0.2] [source] [64-bit] [smp:8:8] [ds:8:8:10] [async-threads:1] [hipe]
+
+  Compiling 1 file (.ex)
+  Interactive Elixir (1.10.3) - press Ctrl+C to exit (type h() ENTER for help)
+
+  iex(1)> LookupEmail.start_link()
+  {:ok, #PID<0.143.0>}
+  iex(2)> LookupEmail.check_email("inquiry@sky-energy.co.id")
+  open_connection_to_mx_server: 220-mail.sky-energy.co.id ESMTP Postfix
+
+  send_smtp_command_HELO_HI_to_mx_server : "220 mail.sky-energy.co.id ESMTP Postfix\r\n"
+  send_smtp_command_MAIL_FROM_to_mx_server : "250 mail.sky-energy.co.id\r\n"
+  send_smtp_command_RCPT_TO_to_mx_server : 250 2.1.0 Ok
+
+  {"inquiry@sky-energy.co.id", :exist}
+  iex(3)>
+  ```
+That's it! The final result would be tuple of a pair of email and its status.
---
+++ lib/lookup_email/helper.ex
@@ -0,0 +1,96 @@
+defmodule LookupEmail.Helper do
+
+  def get_mx_server_of_this_email(email) do
+    # fetch domain part of the email
+    mx = String.split(email, "@") |> Enum.at(1)
+    # extract MX server part, from nslookup response (eg : nslook -q=mx gmail.com)
+    {response, code} = System.cmd("nslookup", ["-q=mx", "#{mx}"])
+
+    # 0 = mx exist, 1 = mx not exist
+    case {response, code} do
+      {response, 0} ->
+        [response]
+        |> Enum.at(0)
+        |> String.split("\n")
+        |> Enum.at(4)
+        |> String.split(" ")
+        |> Enum.at(4)
+        |> String.replace_trailing(".", "")
+        |> URI.parse
+        |> Map.fetch!(:path)
+
+      {response, 1} ->
+        exit "Error nslookup: #{response}"
+    end
+
+  end
+
+  def open_connection_to_mx_server(email_mx_server) do
+
+    opts = [:binary, active: false]
+    telnet_port = 25
+
+    case :gen_tcp.connect('#{email_mx_server}', telnet_port, opts) do
+      {:ok, socket} ->
+          {:ok, msg} = :gen_tcp.recv(socket, 0)
+          IO.puts "open_connection_to_mx_server: #{msg}"
+          socket
+      {:error, reason} ->
+          exit "Can not connect to MX-Server: #{reason}"
+      end
+
+  end
+
+  def send_smtp_command_HELO_HI_to_mx_server(socket) do
+
+    :ok = :gen_tcp.send(socket, "HELO HI \r \n")
+    tcp_recv_respons = :gen_tcp.recv(socket, 0)
+
+    case  tcp_recv_respons do
+        {:ok, msg} ->
+          IO.puts "send_smtp_command_HELO_HI_to_mx_server : #{inspect (msg)}"
+          socket
+        other_msg ->
+          exit "send_smtp_command_HELO_HI_to_mx_server: #{inspect (other_msg)}"
+    end
+
+  end
+
+  def send_smtp_command_MAIL_FROM_to_mx_server(socket) do
+
+    :ok = :gen_tcp.send(socket, "MAIL FROM:<dewa19@gmail.com> \r \n")
+    tcp_recv_respons = :gen_tcp.recv(socket, 0)
+
+    case  tcp_recv_respons do
+        {:ok, msg} ->
+          IO.puts "send_smtp_command_MAIL_FROM_to_mx_server : #{inspect (msg)}"
+          socket
+        other_msg ->
+          exit "send_smtp_command_MAIL_FROM_to_mx_server: #{inspect (other_msg)}"
+    end
+
+  end
+
+  def send_smtp_command_RCPT_TO_to_mx_server(socket, email) do
+
+    :ok = :gen_tcp.send(socket, "RCPT TO:<#{email}> \r \n")
+
+    # fetch 4th response (RCPT TO), & will use it to verify the email
+    {:ok, msg} = :gen_tcp.recv(socket, 0)
+
+    rest_mesg_length = byte_size(msg) - 3
+    <<response_code::binary-size(3), _rest::binary-size(rest_mesg_length)>> = msg
+
+    case response_code do
+      "250" ->
+        IO.puts("send_smtp_command_RCPT_TO_to_mx_server : #{msg}")
+        :exist
+
+      _other_code ->
+        IO.puts("send_smtp_command_RCPT_TO_to_mx_server : #{msg}")
+        :not_exist
+    end
+
+  end
+
+end
