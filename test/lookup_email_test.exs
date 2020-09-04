0d20e54fb1f8ff9eb42bb066b89bed8e3f0596dd
Sigit <dewa19@gmail.com>
2020-09-04 14:20:21 -0500

    update unit test

--- test/lookup_email_test.exs
+++ test/lookup_email_test.exs
@@ -5,14 +5,17 @@ defmodule LookupEmailTest do
   describe "test of emails verification" do
     test "a valid email" do
       LookupEmail.start_link()
-      assert LookupEmail.check_email("dewa19@gmail.com") == "Email dewa19@gmail.com does exist"
+      actual = LookupEmail.check_email("inquiry@sky-energy.co.id")
+      expected = {"inquiry@sky-energy.co.id", :exist}
+      assert actual == expected
     end
 
     test "an invalid email" do
       LookupEmail.start_link()
 
-      assert LookupEmail.check_email("dewwwaaaaaaa@yahoo.com") ==
-               "Email dewwwaaaaaaa@yahoo.com doesn't exist"
+      actual = LookupEmail.check_email("qwqwqw213@gmail.com")
+      expected = {"qwqwqw213@gmail.com", :not_exist}
+      assert actual == expected
     end
   end
 end
